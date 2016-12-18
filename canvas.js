fc213d5ce6925047325e6a314285d50b905d4b98
Tamás Orosz <Tams Orosz>
2016-12-18 15:33:32 +0100

    elso kommit

---
+++ canvas.js
@@ -0,0 +1,22 @@
+/**
+ * Created by bombe on 2016. 12. 16..
+ */
+/*var canvas = document.createElement('canvas');
+canvas.id     = "CursorLayer";
+canvas.width  = 1224;
+canvas.height = 768;
+canvas.style.zIndex   = 8;
+canvas.style.position = "absolute";
+canvas.style.border   = "1px solid";
+document.body.appendChild(canvas);
+function width(){
+    document.body.canvas.offsetHeight;
+}*/
+
+function help(){
+    alert("help");
+}
+
+function write(){
+    document.write("Wassup!!!")
+}
---
+++ myContainer.js
@@ -0,0 +1,25 @@
+/*
+function canv() {
+    var mycanvas = document.createElement('canvas');
+    mycanvas.id = "mycanvas";
+    mycanvas.style.cssText = "width:400px;height:600px;border:1px solid #000000;background:white"
+    document.body.appendChild(mycanvas);
+}
+
+function circle(){
+    var width=document.getElementById("mycanvas").offsetwidth;
+    this.x=width/2;
+    this.y=100;
+    this.r=10;
+    var c = document.getElementById("mycanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(y,x,r,0,2*Math.PI)
+    ctx.stroke();
+    document.write(width);
+}
+*/
+function f2() {
+    // body_width=document.body.clientWidth;
+    alert("asdfa");
+}
